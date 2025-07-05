e1e688445c8970a486be44111bb083950558261e
Yash Jangid <yashjangid13062004@gmail.com>
2025-07-05 22:32:47 +0530

    PUT and DELETE added

--- src/main/java/com/yash/eco_project/service/ProductService.java
+++ src/main/java/com/yash/eco_project/service/ProductService.java
@@ -5,7 +5,9 @@ import com.yash.eco_project.model.Product;
 import com.yash.eco_project.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -20,6 +22,28 @@ public class ProductService {
     }
 
     public Product getProductById(int id) {
-        return repo.findById(id).get() ;
+        return repo.findById(id).orElse(null) ;
     }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product) ;
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageDate(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return repo.save(product) ;
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
+//    public List<Product> searchProducts(String keyword) {
+//        return repo.searchProducts(keyword) ;
+//    }
 }
